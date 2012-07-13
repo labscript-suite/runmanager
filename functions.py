a45ce9fcd9d2e08eb59c08e76d5e16bb57257768
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2012-07-13 05:53:07 +0000

    drop_times is now quadspace.

--- functions.py
+++ functions.py
@@ -1,14 +1,13 @@
+from __future__ import division
 from pylab import *
 
-def drop_times(t_min, t_max, n_points, sample_square=True, randomise=False,repeats=1):
-    if sample_square:
-        times = sqrt(linspace(t_min**2,t_max**2,n_points))
-    else:
-        times = linspace(t_min,t_max,n_points)
-    
-    times=repeat(times,repeats)
+def quadspace(t_min, t_max, n_points, randomise=False, repeats=1):
+    times = sqrt(linspace(t_min**2, t_max**2, n_points))
+    times = repeat(times, repeats)
     
     if randomise:
         return times[argsort(rand(n_points*repeats))]
     else:
         return times
+        
+drop_times = quadspace
