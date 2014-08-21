eed668ee32dc161e599c4d60dc669e86b2f12d93
Chris Billington <chrisjbillington@gmail.com>
2014-08-21 15:12:36 -0400

    Qt: Implemented labscript file selection, file editing, output folder selection, resetting of default folder, rollover of new folder each day, and some other minor callbacks.

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 #####################################################################
 #                                                                   #
-# /main.pyw                                                         #
+# __main__.py                                                        #
 #                                                                   #
 # Copyright 2013, Monash University                                 #
 #                                                                   #
@@ -36,7 +36,7 @@ import labscript_utils.shared_drive as shared_drive
 import runmanager
 import zprocess
 from qtutils.outputbox import OutputBox
-from qtutils import inmain, inmain_later, UiLoader, inthread
+from qtutils import inmain, inmain_later, inmain_decorator, UiLoader, inthread
 import qtutils.icons
 
 # Set working directory to runmanager folder, resolving symlinks
@@ -75,12 +75,10 @@ def setup_logging():
     logger.setLevel(logging.DEBUG)
     return logger
 
-def error_dialog(message):
-    raise NotImplementedError
+@inmain_decorator()
+def error_dialog(parent, message):
+    QtGui.QMessageBox.warning(parent, 'runmanager', message)
 
-def error_dialog_from_thread(message):
-    raise NotImplementedError
-    
 def mkdir_p(path):
     try:
         os.makedirs(path)
@@ -88,6 +86,21 @@ def mkdir_p(path):
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
         else: raise
+        
+def delete_folder_if_empty(folder):
+    if folder is None:
+        return
+    try:
+        os.rmdir(folder)
+    except OSError:
+        pass
+            
+@inmain_decorator()
+def qstring_to_unicode(qstring):
+    if sys.version > '3':
+        return str(qstring.toUtf8(), encoding="UTF-8")
+    else:
+        return unicode(qstring.toUtf8(), encoding="UTF-8")
         
 class FingerTabBarWidget(QtGui.QTabBar):
     """A TabBar with the tabs on the left and the text horizontal.
@@ -207,7 +220,6 @@ class GroupTab(object):
 class RunManager(object):
     def __init__(self):
     
-    
         loader = UiLoader()
         loader.registerCustomWidget(FingerTabWidget)
         self.ui = loader.load('main.ui')
@@ -216,11 +228,14 @@ class RunManager(object):
         self.setup_config()
         self.setup_axes_tab()
         self.setup_groups_tab()
+        self.connect_signals()
         
         # The last location from which a labscript file was selected, defaults to labscriptlib:
         self.last_opened_labscript_folder = self.exp_config.get('paths','labscriptlib')
         # The last location from which a globals file was selected, defaults to experiment_shot_storage:
         self.last_opened_globals_folder = self.exp_config.get('paths', 'experiment_shot_storage')
+        # The last manually selected shot output folder:
+        self.last_selected_shot_output_folder = self.exp_config.get('paths', 'experiment_shot_storage')
         self.shared_drive_prefix = self.exp_config.get('paths', 'shared_drive')
         self.experiment_shot_storage = self.exp_config.get('paths','experiment_shot_storage')
         
@@ -255,6 +270,124 @@ class RunManager(object):
         self.groups_model.setHorizontalHeaderLabels(['Open/Close','Delete','Active','File/groups'])
         self.ui.treeView_groups.setModel(self.groups_model)
     
+    def connect_signals(self):
+        # labscript file and folder selection stuff:
+        self.ui.toolButton_select_labscript_file.clicked.connect(self.on_select_labscript_file_clicked)
+        self.ui.toolButton_select_shot_output_folder.clicked.connect(self.on_select_shot_output_folder_clicked)
+        self.ui.toolButton_edit_labscript_file.clicked.connect(self.on_edit_labscript_file_clicked)
+        self.ui.toolButton_reset_shot_output_folder.clicked.connect(self.on_reset_shot_output_folder_clicked)
+        self.ui.lineEdit_labscript_file.textChanged.connect(self.on_labscript_file_text_changed)
+        self.ui.lineEdit_shot_output_folder.textChanged.connect(self.on_shot_output_folder_text_changed)
+        
+        # compile/send to mise toggling:
+        self.ui.radioButton_compile.toggled.connect(self.on_compile_toggled)
+        self.ui.radioButton_send_to_mise.toggled.connect(self.on_send_to_mise_toggled)
+        
+        # Control buttons; engage, abort, restart subprocess:
+        self.ui.pushButton_engage.clicked.connect(self.on_engage_clicked)
+        self.ui.pushButton_abort.clicked.connect(self.on_abort_clicked)
+        self.ui.pushButton_restart_subprocess.clicked.connect(self.on_restart_subprocess_clicked)
+        
+    def on_select_labscript_file_clicked(self, checked):
+        labscript_file = QtGui.QFileDialog.getOpenFileName(self.ui,
+                                                     'Select labscript file',
+                                                     self.last_opened_labscript_folder,
+                                                     "Python files (*.py)")
+        # Convert to standard platform specific path, otherwise Qt likes forward slashes:
+        labscript_file = qstring_to_unicode(labscript_file)
+        labscript_file = os.path.abspath(labscript_file)
+        # Save the containing folder for use next time we open the dialog box:
+        self.last_opened_labscript_folder = os.path.dirname(labscript_file)
+        # Write the file to the lineEdit:
+        self.ui.lineEdit_labscript_file.setText(labscript_file)
+        # Tell the output folder thread that the output folder might need updating:
+        self.output_folder_update_required.set()
+    
+    def on_edit_labscript_file_clicked(self, checked):
+        # get path to text editor
+        editor_path = self.exp_config.get('programs','text_editor')
+        editor_args = self.exp_config.get('programs','text_editor_arguments')
+        # Get the current labscript file:
+        current_labscript_file = self.ui.lineEdit_labscript_file.text()
+        current_labscript_file = qstring_to_unicode(current_labscript_file)
+        # Ignore if no file selected
+        if not current_labscript_file:
+            return
+        if not editor_path:
+            error_dialog(self.ui, "No editor specified in the labconfig")
+        if '{file}' in editor_args:
+            # Split the args on spaces into a list, replacing {file} with the labscript file
+            editor_args = [arg if arg != '{file}' else current_labscript_file for arg in editor_args.split()]
+        else:
+            # Otherwise if {file} isn't already in there, append it to the other args:
+            editor_args = [current_labscript_file] + editor_args.split()
+        try:
+            subprocess.Popen([editor_path] + editor_args)
+        except Exception as e:
+            error_dialog(self.ui, "Unable to launch text editor specified in %s. Error was: %s"%(self.exp_config.config_path, str(e)))
+        
+    def on_select_shot_output_folder_clicked(self, checked):
+        shot_output_folder = QtGui.QFileDialog.getExistingDirectory(self.ui,
+                                                     'Select shot output folder',
+                                                     self.last_selected_shot_output_folder)
+        # Convert to standard platform specific path, otherwise Qt likes forward slashes:
+        shot_output_folder = qstring_to_unicode(shot_output_folder)
+        shot_output_folder = os.path.abspath(shot_output_folder)
+        # Save the containing folder for use next time we open the dialog box:
+        self.last_selected_shot_output_folder = os.path.dirname(shot_output_folder)
+        # Write the file to the lineEdit:
+        self.ui.lineEdit_shot_output_folder.setText(shot_output_folder)
+        # Tell the output folder rollover thread to run an iteration,
+        # so that it notices this change (even though it won't do anything now - this is so
+        # it can respond correctly if anything else interesting happens within the next second):
+        self.output_folder_update_required.set()
+    
+    def on_reset_shot_output_folder_clicked(self, checked):
+        current_default_output_folder = self.get_default_output_folder()
+        if current_default_output_folder is None:
+            return
+        mkdir_p(current_default_output_folder)
+        self.ui.lineEdit_shot_output_folder.setText(current_default_output_folder)
+        # Tell the output folder rollover thread to run an iteration,
+        # so that it notices this change (even though it won't do anything now - this is so
+        # it can respond correctly if anything else interesting happens within the next second):
+        self.output_folder_update_required.set()
+    
+    def on_labscript_file_text_changed(self, text):
+        enabled = bool(text)
+        self.ui.toolButton_edit_labscript_file.setEnabled(enabled)
+            
+    def on_shot_output_folder_text_changed(self, text):
+        if qstring_to_unicode(text) == self.get_default_output_folder():
+            enabled = False
+        else:
+            enabled = True
+        self.ui.toolButton_reset_shot_output_folder.setEnabled(enabled)
+    
+    def on_compile_toggled(self, checked):
+        if checked:
+            # Show the corresponding page of the stackedWidget
+            page = self.ui.stackedWidgetPage_compile
+            self.ui.stackedWidget_compile_or_mise.setCurrentWidget(page)
+            
+    def on_send_to_mise_toggled(self, checked):
+        if checked:
+            # Show the corresponding page of the stackedWidget
+            page = self.ui.stackedWidgetPage_send_to_mise
+            self.ui.stackedWidget_compile_or_mise.setCurrentWidget(page)
+    
+    def on_engage_clicked(self):
+        raise NotImplementedError
+        
+    def on_abort_clicked(self):
+        raise NotImplementedError
+        
+    def on_restart_subprocess_clicked(self):
+        self.child.terminate()
+        self.from_child.put(['done', False])
+        self.to_child, self.from_child, self.child = zprocess.subprocess_with_queues('batch_compiler.py', self.output_box.port)
+        
+    @inmain_decorator()    
     def get_default_output_folder(self):
         """Returns what the default output folder would be right now,
         based on the current date and selected labscript file.
@@ -262,17 +395,15 @@ class RunManager(object):
         Does not create the default output folder, does not check if it exists."""
         sep = os.path.sep
         current_day_folder_suffix = time.strftime('%Y'+sep+'%m'+sep+'%d')
-        current_labscript_file = inmain(self.ui.lineEdit_labscript_file.text)
+        current_labscript_file = self.ui.lineEdit_labscript_file.text()
+        current_labscript_file = qstring_to_unicode(current_labscript_file)
         if not os.path.exists(current_labscript_file):
             return None
         current_labscript_basename = os.path.splitext(os.path.basename(current_labscript_file))[0]
-        default_output_folder = os.path.join(self.experiment_shot_storage, labscript_basename, self.current_day_dir_suffix)
+        default_output_folder = os.path.join(self.experiment_shot_storage, 
+                                    current_labscript_basename, current_day_folder_suffix)
         return default_output_folder
     
-    def delete_folder_if_empty(self, folder):
-        if os.path.isdir(folder) and not os.listdir(folder):
-            os.unlink(folder)
-                    
     def rollover_shot_output_folder(self):
         """Runs in a thread, checking once a second if it is a new day or the 
         labscript file has changed. If it is or has, creates a new folder in 
@@ -286,25 +417,36 @@ class RunManager(object):
             # Wait up to one second, shorter if the Event() gets set() by someone:
             self.output_folder_update_required.wait(1)
             self.output_folder_update_required.clear()
-            current_default_output_folder = self.get_default_output_folder()
-            if current_default_output_folder is None:
-                # No labscript file selected, or does not exist:
-                continue
-            currently_selected_output_folder = inmain(self.ui.lineEdit_shot_output_folder.text)
-            # If the currently selected output folder does not exist, go back to default:
-            if not os.path.isdir(currently_selected_output_folder):
-                inmain(self.ui.lineEdit_shot_output_folder.setText, current_default_output_folder)
-                # Ensure the default does exist:
-                mdir_p(current_default_output_folder)
-            if current_default_output_folder != previous_default_output_folder:
-                # It's a new day, or a new labscript file!
-                delete_folder_if_empty(previous_default_output_folder)
-                # Is the user even using default folders?
-                if currently_selected_output_folder == previous_default_output_folder:
-                    # Yes they are! In that case, update to use the new folder:
-                    mdir_p(current_default_output_folder)
-                    inmain(self.ui.lineEdit_shot_output_folder.setText,current_default_output_folder)
-                    
+            previous_default_output_folder = self.check_output_folder_update(previous_default_output_folder)
+            
+    @inmain_decorator()
+    def check_output_folder_update(self, previous_default_output_folder):
+        """Do a single check of whether the output folder needs updating.
+        This is implemented as a separate function to the above loop so that
+        the whole check happens at once in the Qt main thread and hence is atomic
+        and can't be interfered with by other Qt calls in the program."""
+        current_default_output_folder = self.get_default_output_folder()
+        if current_default_output_folder is None:
+            # No labscript file selected, or does not exist:
+            return previous_default_output_folder
+        currently_selected_output_folder = self.ui.lineEdit_shot_output_folder.text()
+        currently_selected_output_folder = qstring_to_unicode(currently_selected_output_folder)
+        # If the currently selected output folder does not exist, go back to default:
+        if not os.path.isdir(currently_selected_output_folder):
+            self.ui.lineEdit_shot_output_folder.setText(current_default_output_folder)
+            # Ensure the default does exist:
+            mkdir_p(current_default_output_folder)
+        if current_default_output_folder != previous_default_output_folder:
+            # It's a new day, or a new labscript file!
+            delete_folder_if_empty(previous_default_output_folder)
+            # Is the user even using default folders?
+            if currently_selected_output_folder == previous_default_output_folder:
+                # Yes they are! In that case, update to use the new folder:
+                mkdir_p(current_default_output_folder)
+                self.ui.lineEdit_shot_output_folder.setText(current_default_output_folder)
+            return current_default_output_folder
+        return previous_default_output_folder
+        
     def on_window_destroy(self,widget):
         raise NotImplementedError
     
@@ -323,20 +465,6 @@ class RunManager(object):
     def update_parent_checkbox_by_file(self,filepath):
         raise NotImplementedError
         
-    def on_kill_child_clicked(self, *ignore):
-        self.child.terminate()
-        self.from_child.put(['done', False])
-        self.to_child, self.from_child, self.child = zprocess.subprocess_with_queues('batch_compiler.py', self.output_box.port) 
-        
-    def toggle_compile_or_mise(self, widget):
-        raise NotImplementedError
-            
-    def toggle_view(self,widget):
-        raise NotImplementedError
-            
-    def toggle_run(self,widget):
-        raise NotImplementedError
-    
     def on_new_file_clicked(self,*args):
         raise NotImplementedError
             
@@ -366,38 +494,12 @@ class RunManager(object):
     def close_tab(self,filepath,group_name):
         raise NotImplementedError
         
-    def labscript_file_selected(self,chooser):
-        raise NotImplementedError
-    
-    def on_reset_dir(self,widget):
-        raise NotImplementedError
-    
-    def on_edit_script(self,widget):
-        raise NotImplementedError
-    
-    def update_output_dir(self):
-        raise NotImplementedError
-    
-    def mk_output_dir(self,filename,force_set=False):
-        raise NotImplementedError
-    
-    def labscript_selection_changed(self, chooser):
-        # Probably not required anymore, because Qt probably won't 
-        # unselect a deleted and recreated file like GTK does:
-        raise NotImplementedError
-        
     def on_keypress(self, widget, event):
         raise NotImplementedError
     
-    def engage(self, *args):
-        raise NotImplementedError
-        
     def compile_loop(self):
         raise NotImplementedError
         
-    def on_abort_clicked(self, *args):
-        raise NotImplementedError
-    
     def update_active_groups(self):
         raise NotImplementedError
                     
