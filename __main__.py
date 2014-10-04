80f99976829a532a9b75e995e043c7de039b15f5
Chris Billington <chrisjbillington@gmail.com>
2014-10-04 04:52:59 -0400

    Qt: Resolves issue #29: Standard output and standard error of subprocess out of sync. Actually, issue solved in zprocess and qtutils, simply bumped version requirements here.

--- __main__.py
+++ __main__.py
@@ -67,8 +67,8 @@ def check_version(module_name, at_least, less_than, version=None):
             '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
 
 check_version('labscript_utils', '1.1', '2')
-check_version('qtutils', '1.5.1', '2')
-check_version('zprocess', '1.1.2', '2')
+check_version('qtutils', '1.5.3', '2')
+check_version('zprocess', '1.1.5', '2')
 
 import zprocess.locking
 from zmq import ZMQError
