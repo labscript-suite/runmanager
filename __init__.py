e0d48fb35eca526e6b314bc66521078012ec5dbc
Chris Billington <chrisjbillington@gmail.com>
2014-11-20 11:34:59 -0500

    Float division by default for global expression evaluation

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@
 #                                                                   #
 #####################################################################
 
+from __future__ import division
 import itertools
 import os
 import sys
