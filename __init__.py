c47efc071111d3336db5ff70683c2ffec7eae04c
Phil Starkey <philipstarkey@users.noreply.github.com>
2015-06-10 15:18:15 +1000

    Added try/except around mise import when parsing globals as mise is no longer installed by default when using the labscript suite installer.

--- __init__.py
+++ __init__.py
@@ -357,7 +357,7 @@ def evaluate_globals(sequence_globals, raise_exceptions=True):
     sandbox = TraceDictionary()
     exec('from pylab import *', sandbox, sandbox)
     exec('from runmanager.functions import *', sandbox, sandbox)
-    exec('from mise import MiseParameter', sandbox, sandbox)
+    exec('try: from mise import MiseParameter\nexcept: pass', sandbox, sandbox)
     globals_to_eval = all_globals.copy()
     previous_errors = -1
     while globals_to_eval:
