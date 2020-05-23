637c2b1026c7da9a4491d7f8d232ec747f27a66e
chrisjbillington <chrisjbillington@gmail.com>
2020-05-23 16:24:28 -0400

    Remove two remaining `__future__` imports

--- runmanager/functions.py
+++ runmanager/functions.py
@@ -11,8 +11,6 @@
 #                                                                   #
 #####################################################################
 
-from __future__ import division, unicode_literals, print_function, absolute_import
-
 from pylab import *
 
 ns = 1e-9
